9016f9b45a8548c34056f2de514c741151c0ed98
Dimos Gkontevas <dimosgkontevas1@gmail.com>
2025-02-24 21:27:14 +0200

    new commit

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,67 +1,93 @@
-"use client"
-import React from 'react'
-import { useTransition, useState } from 'react'
-import Image from 'next/image'
-import TabButton from './TabButton'
-import { TAB_DATA } from '@/data/Data'
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+      <li>React</li>
+    <li>Next.js</li>
+    <li>Python</li>
+    <li>MongoDB</li>
+      <li>Javascript</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>On-going Studies at IEK EUROPROODOS.</li>
+        <li>On-going workshops, seminars and online courses.</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+       <li>E.C.D.L. Certificate</li>
+       <li>Senior High School Lykeio Lehaiou Certificate</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
-    const [tab, setTab] = useState("skills")
-    const [isPending, startTransition] = useTransition()
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
 
-    const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id)
-        })
-    }
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
 
-    const tabContent = React.useMemo(() => {
-        return TAB_DATA.find((t) => t.id === tab)?.content || <p>No content available</p>
-    }, [tab])
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png"  alt="about-image" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-purple-700 mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I'm a 20 year old beginner developer with a big passion about coding and the world of technology. Trying to improve everyday and get the better out of myself! Below you can check my education and skills.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section className="text-white" id="about">
-            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-7 sm:px-10 xl:gap-16 sm:py-16 xl:px-16">
-                <Image 
-                    src="/images/about-image.png" 
-                    alt="code-workspace" 
-                    width={1000} 
-                    height={1000} 
-                />
-                <div className="mt-10 md:mt-0 text-left flex flex-col h-full">
-                    <h2 className='text-5xl font-bold text-[#8a00c4] mb-4 flex justify-center'>
-                        About Me
-                    </h2>
-                    <p className='text-base lg:text-xl'>
-                        I'm a 20 year old beginner developer with a big passion about coding and the world of technology. Trying to improve everyday and get the better out of myself! Below you can check my education and skills.
-                    </p>
-                    <div className='flex flex-row justify-start mt-9'>
-                        <TabButton 
-                            selectTab={() => handleTabChange("skills")} 
-                            active={tab === "skills"}
-                        >
-                            Skills
-                        </TabButton>
-                        <TabButton 
-                            selectTab={() => handleTabChange("education")} 
-                            active={tab === "education"}
-                        >
-                            Education
-                        </TabButton>
-                        <TabButton 
-                            selectTab={() => handleTabChange("Certificates")} 
-                            active={tab === "Certificates"}
-                        >
-                            Certificates
-                        </TabButton>
-                    </div>
-                    <div className='mt-8'>
-                        {tabContent}
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
-
-export default AboutSection
+export default AboutSection;
